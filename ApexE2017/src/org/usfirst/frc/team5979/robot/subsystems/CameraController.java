358dd16c8e95fa6151fcffece9e64d36aa25250b
GhostMonk <Liam@192.168.1.104>
2017-01-08 01:50:10 -0500

    Added USB camera support.

---
+++ ApexE2017/src/org/usfirst/frc/team5979/robot/subsystems/CameraController.java
@@ -0,0 +1,31 @@
+package org.usfirst.frc.team5979.robot.subsystems;
+
+import edu.wpi.first.wpilibj.CameraServer;
+
+/**
+ * Initializes the default USB camera and enables it for basic streaming to
+ * the dashboard.
+ * @version 1.0
+ * @author Liam Williams
+ */
+public class CameraController {
+	CameraServer camera;
+	public CameraController() {
+		camera = CameraServer.getInstance();
+	}
+	/**
+	 * Initializes the camera with default settings.
+	 */
+	public void init() {
+		camera.setQuality(50);
+		camera.startAutomaticCapture();
+	}
+	/**
+	 * Initializes the camera with custom quality.
+	 * @param quality Camera quality to be selected.
+	 */
+	public void init(int quality) {
+		camera.setQuality(quality);
+		camera.startAutomaticCapture();
+	}
+}
